89548a919ad18718891d672f8cae88cc9139d4b5
mcostaflor <matheuscostaflor@gmail.com>
2021-08-05 20:15:29 -0300

    add missing toastity css file to app

--- cadastro-usuario/src/index.js
+++ cadastro-usuario/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+
+import 'react-toastify/dist/ReactToastify.css';
 import "antd/dist/antd.css";
 
 // import '@testing-library/jest-dom';
