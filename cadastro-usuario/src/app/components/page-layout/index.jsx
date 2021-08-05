9f420e7e0f2fbf4274db63b067e8c109ff3ca3ad
mcostaflor <matheuscostaflor@gmail.com>
2021-08-05 20:14:30 -0300

    update page layout component for mobile environments

--- cadastro-usuario/src/app/components/page-layout/index.jsx
+++ cadastro-usuario/src/app/components/page-layout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { Breadcrumbs } from '../breadcrumb';
+import { BrowserView } from 'react-device-detect';
 import './index.scss';
 
 const { Content, Footer } = Layout;
@@ -8,15 +9,17 @@ const { Content, Footer } = Layout;
 export const PageLayout = ({ children, breadcrumbs }) => {
     return (
         <Layout className="page-layout">
-            <div className="page-layout__breadcrumbs">
-                <Breadcrumbs
-                    items={breadcrumbs}
-                />
-            </div>
+            <BrowserView>
+                <div className="page-layout__breadcrumbs">
+                    <Breadcrumbs
+                        items={breadcrumbs}
+                    />
+                </div>
+            </BrowserView>
             <Content className="page-layout__content">
                 {children}
             </Content>
-            <Footer>
+            <Footer className="page-layout__footer">
                 mcostaflor (r) 2021
             </Footer>
         </Layout>
