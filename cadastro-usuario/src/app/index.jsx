6d7ae2cf67b1c142727e9274541d097b77281dfb
mcostaflor <matheuscostaflor@gmail.com>
2021-08-05 20:14:44 -0300

    add toast support

--- cadastro-usuario/src/app/index.jsx
+++ cadastro-usuario/src/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Navbar } from './components/navbar';
 import { HomePage } from './pages/home';
 import { UsersPage } from './pages/users';
@@ -10,6 +11,7 @@ function App() {
     return (
         <div className="app">
             <BrowserRouter>
+                <ToastContainer />
                 <Navbar />
                 <Switch>
                     <Route exact path="/" render={() => <HomePage />} />
