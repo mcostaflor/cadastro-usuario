6b63604f40247b2197b5b79ba6eac5e8850def6a
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 08:58:33 -0300

    change feedback text

--- cadastro-usuario-backend/orm.js
+++ cadastro-usuario-backend/orm.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('postgres://xmwcvzef:N8q8_tmoFvS2Q-aI344YeZsVoZoj-AB-@chunee.db.elephantsql.com/xmwcvzef');
 
 sequelize.authenticate()
-    .then(() => console.log('Connection has been established successfully.'))
+    .then(() => console.log('Connected to Postgres.'))
     .catch(error => console.error('Unable to connect to the database:', error));
 
 module.exports = sequelize;
