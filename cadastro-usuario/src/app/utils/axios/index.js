66e45aff3ae4f08ebecccb14f897bedfefe0b179
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 08:55:24 -0300

    add default json header to axios

--- cadastro-usuario/src/app/utils/axios/index.js
+++ cadastro-usuario/src/app/utils/axios/index.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import config from '../../../config.json';
 
 const instance = axios.create({
-    baseURL: config.api.baseUrl
+    baseURL: config.api.baseUrl,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 });
 
 export default instance;
