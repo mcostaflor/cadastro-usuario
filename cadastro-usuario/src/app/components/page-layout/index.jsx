c33997c8d200f1001309d9d6a8d0dcdecc4c921c
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 20:38:28 -0300

    remove breadcrumbs

--- cadastro-usuario/src/app/components/page-layout/index.jsx
+++ cadastro-usuario/src/app/components/page-layout/index.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Breadcrumbs } from '../breadcrumb';
-import { BrowserView } from 'react-device-detect';
 import './index.scss';
 
 const { Content, Footer } = Layout;
 
-export const PageLayout = ({ children, breadcrumbs }) => {
+export const PageLayout = ({ children }) => {
     return (
         <Layout className="page-layout">
-            <BrowserView>
-                <div className="page-layout__breadcrumbs">
-                    <Breadcrumbs
-                        items={breadcrumbs}
-                    />
-                </div>
-            </BrowserView>
             <Content className="page-layout__content">
                 {children}
             </Content>
