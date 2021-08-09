5c14d90a83c33e22047965b5bc899d0da7aaf423
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 20:39:23 -0300

    add home navigation cards

--- cadastro-usuario/src/app/pages/home/index.jsx
+++ cadastro-usuario/src/app/pages/home/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Col, Divider, Row, Typography } from 'antd';
 import { PageLayout } from '../../components/page-layout';
+import { HomeCard } from './components/home-card';
+import { user } from '../../routes/index';
+
+const { Title } = Typography;
 
 export const HomePage = () => {
     return (
-        <PageLayout
-            breadcrumbs={[
-                {
-                    route: '/',
-                    text: 'Home'
-                },
-            ]}
-        >
-            Home
+        <PageLayout>
+            <Title level={1}>
+                Home
+            </Title>
+            <Divider />
+            <Row>
+                <Col xs={24} sm={12} md={8} lg={6}>
+                    <HomeCard
+                        id="home-card"
+                        icon={"users"}
+                        title="Users"
+                        body="Manage system users"
+                        route={user.list}
+                    />
+                </Col>
+            </Row>
         </PageLayout>
     )
 }
