2247f5ecdd0f06b90178e26321e0fb70f1cbf65b
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 08:54:59 -0300

    remove contenttype header

--- cadastro-usuario/src/app/api/user.js
+++ cadastro-usuario/src/app/api/user.js
@@ -4,17 +4,9 @@ const controllerUrl = '/users';
 
 const api = {
     list: async () => (await (axios.get(controllerUrl))).data,
-    insert: async payload => (await axios.put(controllerUrl, payload, {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    })).data,
+    insert: async payload => (await axios.post(controllerUrl, payload)).data,
     delete: async (id) => (await axios.delete(`${controllerUrl}/${id}`)).data,
-    update: async (id, payload) => (await axios.put(`${controllerUrl}/${id}`, payload, {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    })).data,
+    update: async (id, payload) => (await axios.put(`${controllerUrl}/${id}`, payload)).data,
     get: async (id) => (await axios.get(`${controllerUrl}/${id}`)).data,
 };
 
