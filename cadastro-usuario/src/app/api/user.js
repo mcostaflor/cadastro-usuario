4fecb79b07b2e48d991abddd5103a2575b3d3fe5
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 00:12:02 -0300

    add header to accept files on reequest

--- cadastro-usuario/src/app/api/user.js
+++ cadastro-usuario/src/app/api/user.js
@@ -4,9 +4,17 @@ const controllerUrl = '/users';
 
 const api = {
     list: async () => (await (axios.get(controllerUrl))).data,
-    insert: async payload => (await axios.put(controllerUrl, payload)).data,
+    insert: async payload => (await axios.put(controllerUrl, payload, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })).data,
     delete: async (id) => (await axios.delete(`${controllerUrl}/${id}`)).data,
-    update: async (id, payload) => (await axios.put(`${controllerUrl}/${id}`, payload)).data,
+    update: async (id, payload) => (await axios.put(`${controllerUrl}/${id}`, payload, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })).data,
     get: async (id) => (await axios.get(`${controllerUrl}/${id}`)).data,
 };
 
