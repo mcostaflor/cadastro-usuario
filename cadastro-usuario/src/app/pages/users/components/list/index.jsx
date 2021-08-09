3f9e0646da2632815836d66796fa5524e16c182d
mcostaflor <matheuscostaflor@gmail.com>
2021-08-09 00:13:18 -0300

    improve user list page

--- cadastro-usuario/src/app/pages/users/components/list/index.jsx
+++ cadastro-usuario/src/app/pages/users/components/list/index.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Button, Divider, Table } from 'antd';
-import { UserAddOutlined } from '@ant-design/icons';
+
+import { Typography, Button, Divider, Table, Space, Pagination, message, Empty, Spin } from 'antd';
+import { ArrowLeftOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Link, useHistory } from 'react-router-dom';
+
+import { MobileOnlyView, BrowserView } from 'react-device-detect';
+
+import { userApi } from '../../../../api';
+
+import { user } from '../../../../routes'
+import { UserCard } from '../../../../components/user-card';
+
+import { paginate } from '../../../../helpers/paginate';
+
 
 import './index.scss';
-import { Link } from 'react-router-dom';
 
 const { Title } = Typography;
 
@@ -23,32 +34,43 @@ const columns = [
         dataIndex: 'birthdate',
         key: 'birthdate',
     },
-];
-
-const mockData = [
     {
-        code: 50230,
-        name: 'Matheus Flor',
-        birthdate: '06/26/1997',
-    }
+        render: userData => <Link to={user.page.replace(":id", userData.code)}>View</Link>,
+    },
 ];
 
-export const UserList = () => {
+const PAGE_SIZE = 5;
+
+export const Users = () => {
     const [isLoadingUsers, setIsLoadingUsers] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
     const [users, setUsers] = useState([]);
 
+    const history = useHistory();
+
     useEffect(() => {
-        try {
-            setIsLoadingUsers(true);
-        } catch (e) {
-            
-        } finally {
-            setTimeout(() => {
+        async function fetchUsers() {
+            try {
+                setIsLoadingUsers(true);
+                const result = await userApi.list();
+                setUsers(result);
+            } catch (e) {
+                message.error("Failed to fetch user list");
+            } finally {
                 setIsLoadingUsers(false);
-                setUsers(mockData.concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData).concat(mockData));
-            }, 2000);
-        }
-    }, [])
+            };
+        };
+
+        fetchUsers();
+    }, []);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [currentPage]);
+
+    const handleUserClicked = (code) => {
+        history.push(user.page.replace(":id", code));
+    };
 
     return (
         <div>
@@ -57,21 +79,79 @@ export const UserList = () => {
                     Users
                 </Title>
                 <div className={'user-list__header__actions'}>
-                    <Link to="/users/new">
-                        <Button type="primary" shape="circle" size="large" icon={<UserAddOutlined />} />
-                    </Link>
+                    <Space size="small">
+                        <Link to="/">
+                            <Button
+                                type="secondary"
+                                shape="circle"
+                                size="large"
+                                icon={<ArrowLeftOutlined />}
+                            />
+                        </Link>
+                        <Link to="/users/new">
+                            <Button
+                                type="primary"
+                                shape="circle"
+                                size="large"
+                                icon={<UserAddOutlined />}
+                                disabled={isLoadingUsers}
+                                loading={isLoadingUsers}
+                            />
+                        </Link>
+                    </Space>
                 </div>
             </div>
             <Divider />
-            <Table
-                dataSource={users}
-                columns={columns}
-                size="middle"
-                loading={isLoadingUsers}
-                pagination={{
-                    pageSize: 5,
-                }}
-            />
+            <BrowserView>
+                <Table
+                    dataSource={users}
+                    columns={columns}
+                    size="middle"
+                    loading={isLoadingUsers}
+                    pagination={{
+                        pageSize: PAGE_SIZE,
+                        current: currentPage,
+                        onChange: page => setCurrentPage(page),
+                    }}
+                />
+            </BrowserView>
+            <MobileOnlyView>
+                {users.length > 0 ?
+                    <>
+                        <Pagination
+                            current={currentPage}
+                            total={users.length}
+                            pageSize={PAGE_SIZE}
+                            onChange={page => setCurrentPage(page)}
+                            responsive
+                            style={{
+                                marginBottom: 24
+                            }}
+                        />
+                        <div>
+                            {paginate(users, currentPage, PAGE_SIZE).map((user, index) => (
+                                <UserCard
+                                    key={user.code + index}
+                                    name={user.name + index}
+                                    birthdate={user.birthdate}
+                                    code={user.code}
+                                    onClick={() => handleUserClicked(user.code)}
+                                />
+                            ))}
+                        </div>
+                    </>
+                    :
+                    <>
+                        {isLoadingUsers ?
+                            <Spin
+                                size="large"
+                            />
+                            :
+                            <Empty />
+                        }
+                    </>
+                }
+            </MobileOnlyView>
         </div>
     )
 }
